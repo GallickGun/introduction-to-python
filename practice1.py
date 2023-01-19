39d210b02cf87a10384bd0a342a22c2eaac579a7
rktGallickGun <hans@rukita.co>
2023-01-19 21:19:08 +0700

    homework practice

--- practice1.py
+++ practice1.py
@@ -11,3 +11,15 @@ print('Room temp is: ', fahrenheit, "Fahrenheit")
 
 kelvin = celcius + 273
 print('Room temp is: ', kelvin, "Kelvin")
+
+fahrenheitInput = float(input("Insert temperature in Fahrenheit: "))
+print("Room temp is: ", fahrenheitInput, "Fahrenheit")
+
+fahrenheitToKelvin = (fahrenheitInput + 459.67) * 5/9
+print('Room temp is: ', fahrenheitToKelvin, "Kelvin")
+
+kelvinInput = float(input("Insert temperature in Kelvin: "))
+print('Room temp is: ', kelvinInput, "Kelvin")
+
+kelvinToFahrenheit = (kelvinInput - 273.15) * 9/5 + 32
+print('Room temp is: ', kelvinToFahrenheit, "Fahrenheit")
