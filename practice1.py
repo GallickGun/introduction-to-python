ba8e855974003aa5846c610c1a0a4987a29a3109
rktGallickGun <hans@rukita.co>
2023-01-19 21:01:47 +0700

    simple practice

---
+++ practice1.py
@@ -0,0 +1,13 @@
+print("Temperature conversion")
+
+celcius = float(input("Insert temperature in Celcius: "))
+print('Room temp is: ', celcius, "Celcius")
+
+reamur = (4/5) * celcius
+print('Room temp is: ', reamur, "Reamur")
+
+fahrenheit = ((4/5) * celcius) +32
+print('Room temp is: ', fahrenheit, "Fahrenheit")
+
+kelvin = celcius + 273
+print('Room temp is: ', kelvin, "Kelvin")
