5291bcbfdd442a6db5c43a296e1184a29600905a
rktGallickGun <hans@rukita.co>
2023-01-29 17:09:11 +0700

    logic and boolean

---
+++ logic-and-boolean.py
@@ -0,0 +1,18 @@
+# NOT
+a = True
+b = not a
+print('boolean: ', a)
+print('boolean: ', b)
+
+# OR
+c = True
+d = False
+e = c or d
+print(c,'OR',d,'=', e)
+
+# AND
+f = True
+g = not f
+h = f and g
+print(f,'AND',g,'=', h)
+
