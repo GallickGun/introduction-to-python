013bebbfff350817106a55acd0aa4cf44ce4a26a
rktGallickGun <hans@rukita.co>
2023-02-25 21:15:45 +0700

    if else

---
+++ if-else.py
@@ -0,0 +1,11 @@
+name = input("Input your name: ")
+
+
+#inline
+# if name=="GallickGun": print("You're the best!")
+
+#indentation
+if name=="GallickGun":
+        print("You're the best!")
+else:
+        print("You're okay")
