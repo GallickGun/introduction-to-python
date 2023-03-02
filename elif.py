dd0d646792422e4285d2bc61a2a31efdabc6e11c
rktGallickGun <hans@rukita.co>
2023-03-02 20:41:25 +0700

    elif statement

---
+++ elif.py
@@ -0,0 +1,10 @@
+name = input("Input name: ")
+
+if name=="Gallick":
+        print("Welcome, Gallick!")
+elif name=="Gun":
+        print("Welcome, Gun!")
+elif name=="GallickGun":
+        print("Welcome, master!")
+else:
+        print(f'Welcome, {name}!')
